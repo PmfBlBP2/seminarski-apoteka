c185117b640111d7daa0aae82922008b06aa9aba
WINDOWS 10 <WINDOWS 10@DESKTOP-7SFELJ4>
2019-10-12 13:07:31 +0200

    Add contollers for "Racun" and "Kupovina" models

---
+++ src/Apoteka/Controllers/KupovinaController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Apoteka.Models;
+
+namespace Apoteka.Controllers
+{
+    public class KupovinaController : Controller
+    {
+        private readonly ApotekaContext _context;
+
+        public KupovinaController(ApotekaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Kupovina
+        public async Task<IActionResult> Index()
+        {
+            var apotekaContext = _context.Kupovina.Include(k => k.Lijek).Include(k => k.Racun);
+            return View(await apotekaContext.ToListAsync());
+        }
+
+        // GET: Kupovina/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kupovina = await _context.Kupovina
+                .Include(k => k.Lijek)
+                .Include(k => k.Racun)
+                .FirstOrDefaultAsync(m => m.RacunId == id);
+            if (kupovina == null)
+            {
+                return NotFound();
+            }
+
+            return View(kupovina);
+        }
+
+        // GET: Kupovina/Create
+        public IActionResult Create()
+        {
+            ViewData["LijekId"] = new SelectList(_context.Lijek, "LijekId", "LijekId");
+            ViewData["RacunId"] = new SelectList(_context.Racun, "RacunId", "RacunId");
+            return View();
+        }
+
+        // POST: Kupovina/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RacunId,LijekId,Kolicina,Iznos")] Kupovina kupovina)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(kupovina);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LijekId"] = new SelectList(_context.Lijek, "LijekId", "LijekId", kupovina.LijekId);
+            ViewData["RacunId"] = new SelectList(_context.Racun, "RacunId", "RacunId", kupovina.RacunId);
+            return View(kupovina);
+        }
+
+        // GET: Kupovina/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kupovina = await _context.Kupovina.FindAsync(id);
+            if (kupovina == null)
+            {
+                return NotFound();
+            }
+            ViewData["LijekId"] = new SelectList(_context.Lijek, "LijekId", "LijekId", kupovina.LijekId);
+            ViewData["RacunId"] = new SelectList(_context.Racun, "RacunId", "RacunId", kupovina.RacunId);
+            return View(kupovina);
+        }
+
+        // POST: Kupovina/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RacunId,LijekId,Kolicina,Iznos")] Kupovina kupovina)
+        {
+            if (id != kupovina.RacunId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(kupovina);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KupovinaExists(kupovina.RacunId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LijekId"] = new SelectList(_context.Lijek, "LijekId", "LijekId", kupovina.LijekId);
+            ViewData["RacunId"] = new SelectList(_context.Racun, "RacunId", "RacunId", kupovina.RacunId);
+            return View(kupovina);
+        }
+
+        // GET: Kupovina/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var kupovina = await _context.Kupovina
+                .Include(k => k.Lijek)
+                .Include(k => k.Racun)
+                .FirstOrDefaultAsync(m => m.RacunId == id);
+            if (kupovina == null)
+            {
+                return NotFound();
+            }
+
+            return View(kupovina);
+        }
+
+        // POST: Kupovina/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var kupovina = await _context.Kupovina.FindAsync(id);
+            _context.Kupovina.Remove(kupovina);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KupovinaExists(int id)
+        {
+            return _context.Kupovina.Any(e => e.RacunId == id);
+        }
+    }
+}
---
+++ src/Apoteka/Controllers/RacunController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Apoteka.Models;
+
+namespace Apoteka.Controllers
+{
+    public class RacunController : Controller
+    {
+        private readonly ApotekaContext _context;
+
+        public RacunController(ApotekaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Racun
+        public async Task<IActionResult> Index()
+        {
+            var apotekaContext = _context.Racun.Include(r => r.Osiguranik);
+            return View(await apotekaContext.ToListAsync());
+        }
+
+        // GET: Racun/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var racun = await _context.Racun
+                .Include(r => r.Osiguranik)
+                .FirstOrDefaultAsync(m => m.RacunId == id);
+            if (racun == null)
+            {
+                return NotFound();
+            }
+
+            return View(racun);
+        }
+
+        // GET: Racun/Create
+        public IActionResult Create()
+        {
+            ViewData["OsiguranikId"] = new SelectList(_context.Osiguranik, "OsiguranikId", "Jmbg");
+            return View();
+        }
+
+        // POST: Racun/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("RacunId,OsiguranikId,Iznos,DatumIzdavanja")] Racun racun)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(racun);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OsiguranikId"] = new SelectList(_context.Osiguranik, "OsiguranikId", "Jmbg", racun.OsiguranikId);
+            return View(racun);
+        }
+
+        // GET: Racun/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var racun = await _context.Racun.FindAsync(id);
+            if (racun == null)
+            {
+                return NotFound();
+            }
+            ViewData["OsiguranikId"] = new SelectList(_context.Osiguranik, "OsiguranikId", "OsiguranikId", racun.OsiguranikId);
+            return View(racun);
+        }
+
+        // POST: Racun/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("RacunId,OsiguranikId,Iznos,DatumIzdavanja")] Racun racun)
+        {
+            if (id != racun.RacunId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(racun);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!RacunExists(racun.RacunId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["OsiguranikId"] = new SelectList(_context.Osiguranik, "OsiguranikId", "OsiguranikId", racun.OsiguranikId);
+            return View(racun);
+        }
+
+        // GET: Racun/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var racun = await _context.Racun
+                .Include(r => r.Osiguranik)
+                .FirstOrDefaultAsync(m => m.RacunId == id);
+            if (racun == null)
+            {
+                return NotFound();
+            }
+
+            return View(racun);
+        }
+
+        // POST: Racun/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var racun = await _context.Racun.FindAsync(id);
+            _context.Racun.Remove(racun);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool RacunExists(int id)
+        {
+            return _context.Racun.Any(e => e.RacunId == id);
+        }
+    }
+}
